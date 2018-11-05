fc01cbfd31e2d3e3c72b5a623bf499510476847b
Giordanna De Gregoriis <gior.grs@gmail.com>
2018-11-05 14:37:22 -0200

    Updates

---
+++ precache-manifest.33572d4f9da4ce61dee78ebe56d0f482.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/mapa-bairro/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/mapa-bairro/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/mapa-bairro/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/mapa-bairro/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "ed2a7a470c978b4cda37",
+    "url": "/mapa-bairro/static/js/runtime~main.ed2a7a47.js"
+  },
+  {
+    "revision": "e575904a27b6ddaa38f0",
+    "url": "/mapa-bairro/static/js/main.e575904a.chunk.js"
+  },
+  {
+    "revision": "74566da776cd24c05002",
+    "url": "/mapa-bairro/static/js/1.74566da7.chunk.js"
+  },
+  {
+    "revision": "e575904a27b6ddaa38f0",
+    "url": "/mapa-bairro/static/css/main.119c02d3.chunk.css"
+  },
+  {
+    "revision": "8aa1b72ba02ed5e9db83319e3bcb5921",
+    "url": "/mapa-bairro/index.html"
+  }
+];
